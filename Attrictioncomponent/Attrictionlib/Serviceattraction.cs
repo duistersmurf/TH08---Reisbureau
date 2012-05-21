f9f1511ef693ba97a655ae541701f11aa92b5446
ruwan kellaert <ruwankellaert@hotmail.com>
2012-05-21 11:19:08 +0200

    added attraction stub

---
+++ Attrictioncomponent/Attrictionlib/Serviceattraction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Attrictionlib
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Serviceattraction" in both code and config file together.
+    public class Serviceattraction : IServiceattraction
+    {
+        public void GetAvailableAttractions()
+        {
+        }
+        public void GetTickets()
+        {
+        }
+        public bool IsTicketAvailable()
+        {
+            return true;
+        }
+        public void BookAttraction()
+        {
+        }
+    }
+}
